b467feb9b8fd55031358e1da75cea0de9459bde4
anggrainini <rain.aini@gmail.com>
2019-01-23 05:07:21 +0700

    last update

---
+++ application/views/global/profile.php
@@ -0,0 +1,103 @@
+<div class="row">
+                <div class="col-md-12">
+                   <center> <h4 class="page-head-line">Profile Mts N 4 Sleman</h4></center>
+                </div>
+
+<div class="row">
+                <div class="col-md-6">
+                      <div class="notice-board">
+                        <div class="panel panel-info">
+                        <div class="panel-heading">
+                        <i class=" glyphicon glyphicon-picture"></i>
+                            Photo Profile Mts N 4 SLEMAN
+                        </div>
+                                <div class="panel-body">
+
+                               <img src="<?php echo base_url(); ?>assets2/img/1.jpg" width="265" height="240">&nbsp
+                               <img src="<?php echo base_url(); ?>assets2/img/3.jpg" width="265" height="240">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                <div class="col-md-6">
+                      <div class="notice-board">
+                        <div class="panel panel-info">
+                        <div class="panel-heading">
+                        <class="box-title"><i class=" glyphicon glyphicon-home"></i>
+                            Identitas Mts N 4 Sleman
+                        </div>
+                                <div class="panel-body">
+                                <table width="100%">
+                                <tr>
+                                    <td width="60%">
+                              
+                                        <b>Alamat :</b>Jalan Purbaya 24, Tridadi, Sleman<br>
+                                        <b>Telepon :</b>(0274) 868 176 <br>
+                                        <b>Email : </b> slemankota.kabsleman@gmail.com</br>
+                                        <b>NPSN :</b> - <br>
+                                        <b>Status :</b> Negeri<br>
+                                        <b>Akreditasi :</b> - <br>
+                                        <b>Bentuk Pendidikan :</b> Madrasah<br>
+                                        <b>Status Kepemilikan :</b> Pemerintah Daerah<br>
+                                        <b>SK Pendirian Sekolah :</b> -<br>
+                                        <b>Tanggal SK Izin Operasional :</b>-<br>
+                                    </td>
+                                    <td width="40%">
+                                        <b>Tanggal SK Pendirian :</b> -<br>
+                                        <b>Luas Tanah Milik :</b> - M^2<br> 
+                                        <b>SK Izin Operasional :</b> -<br><br><br><br><br><br>
+                                    </td>
+                                 </tr>
+                            </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                      <div class="notice-board">
+                        <div class="panel panel-info">
+                        <div class="panel-heading">
+                        <class="box-title"><i class=" glyphicon glyphicon-th-list"></i>
+                            MISI Mts N 4 Sleman 
+                        </div>
+                                <div class="panel-body">
+                                <ul align="justify">
+                              Visi Madrasah
+
+Mewujudkan kondusifitas madrasah untuk mendidik siswa menjadi beriman, bertakwa, berakhlak mulia, cerdas, mandiri dan bermanfaat.
+
+
+
+
+
+
+                               </ul>  
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                      <div class="notice-board">
+                        <div class="panel panel-info">
+                        <div class="panel-heading">
+                        <class="box-title"><i class=" glyphicon glyphicon-th-list"></i>
+                            VISI Mts N 4 Sleman 
+                        </div>
+                                <div class="panel-body">
+                                <ul align="justify">
+                                Refungsionalisasi, revitalisasi, reorientasi segenap sumber daya serta pemberdayaan stakeholder madrasah berdasarkan prinsip perbaikan terus menerus (continuous improvement) menuju madrasah yang unggul dan kompetitif.
+                               </ul>  
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
